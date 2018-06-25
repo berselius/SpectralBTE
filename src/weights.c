d0b9594c12cafa84e603a6477e92283b62e3c2ae
Hector Carrillo <hcarrillo@lanl.gov>
2018-06-25 09:54:33 -0600

    rebase + fixed weights.c

--- src/weights.c
+++ src/weights.c
@@ -299,6 +299,8 @@ double gHat3(double ki1, double ki2, double ki3, double zeta1, double zeta2, dou
 //this generates the convolution weights G
 void generate_conv_weights_iso(double **conv_weights)
 {
+    int i,j,k,l,m,n;
+
     #pragma omp parallel for private(i,j,k,l,m,n)
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
