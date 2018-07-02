136605adf70782bb2e629f1f3f9f75517e8f807a
Heesoo Kim - 329195 <hkim22@darwin-fe1.lanl.gov>
2018-07-02 12:31:50 -0600

    Flatten nested loops in fft3D

--- src/collisions.c
+++ src/collisions.c
@@ -23,8 +23,11 @@ static int N;
 static double *wtN;
 static double scale3;
 
+static int inverse = 1;
+static int noinverse = 0;
+
 static void find_maxwellians(double *M_mat, double *g_mat, double *mat);
-static void compute_Qhat(double *Q, double **conv_weights);
+static void compute_Qhat(double **conv_weights, double *f_mat, double *g_mat);
 
 //Initializes this module's static variables and allocates what needs allocating
 void initialize_coll(int nodes, double length, double *vel, double *zeta) {
@@ -102,16 +105,16 @@ static void find_maxwellians(double *M_mat, double *g_mat, double *mat) {
   }
 }
 
-static void compute_Qhat(double *Q, double **conv_weights) {
+static void compute_Qhat(double **conv_weights, double *f_mat, double *g_mat) {
   int i, j, k, index, l, m, n, x, y, z;
   double *conv_weight_chunk;
 
   for (index = 0; index < N * N * N; index++) {
     qHat[index][0] = 0.0;
     qHat[index][1] = 0.0;
-    fftIn_f[index][0] = M_i[index];
+    fftIn_f[index][0] = f_mat[index];
     fftIn_f[index][1] = 0.0;
-    fftIn_g[index][0] = g_j[index];
+    fftIn_g[index][0] = g_mat[index];
     fftIn_g[index][1] = 0.0;
   }
 
@@ -159,11 +162,6 @@ static void compute_Qhat(double *Q, double **conv_weights) {
 
   //End of parallel section
   ifft3D(qHat, fftOut_f);
-
-  //set Collision output
-  for (index = 0; index < N * N * N; index++) {
-    Q[index] = fftOut_f[index][0];
-  }
 }
 
 /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
@@ -173,133 +171,25 @@ static void compute_Qhat(double *Q, double **conv_weights) {
   -----------------
   The main function for calculating the collision effects
 */
-void ComputeQ_maxPreserve(double *f, double *g, double *Q, double **conv_weights)
-{
-  int i, j, k, l, m, n, x, y, z, index, index1;
-  double *conv_weight_chunk;
+void ComputeQ_maxPreserve(double *f, double *g, double *Q, double **conv_weights) {
+  int index;
 
   find_maxwellians(M_i, g_i, f);
   find_maxwellians(M_j, g_j, g);
 
-  compute_Qhat(Q, conv_weights);
-
+  compute_Qhat(conv_weights, M_i, g_j);
+  //set Collision output
   for (index = 0; index < N * N * N; index++) {
-    qHat[index][0] = 0.0;
-    qHat[index][1] = 0.0;
-    fftIn_f[index][0] = g_i[index];
-    fftIn_f[index][1] = 0.0;
-    fftIn_g[index][0] = M_j[index];
-    fftIn_g[index][1] = 0.0;
+    Q[index] = fftOut_f[index][0];
   }
 
-  //move to fourier space
-  fft3D(fftIn_f, fftOut_f);
-  fft3D(fftIn_g, fftOut_g);
-
-
-  #pragma omp parallel for private(i,j,k,l,m,n,x,y,z,conv_weight_chunk)
-  for (index = 0; index < N * N * N; index++) {
-    i = index / (N * N);
-    j = (index - i * N * N) / N;
-    k = index - N * (j + i * N);
-    conv_weight_chunk = conv_weights[index];
-
-    int n2 = N / 2;
-    for(l=0;l<N;l++) {
-      x = i + n2 - l;
-      if (x < 0)
-	x = N + x;
-      else if (x > N-1)
-	x = x - N;
-
-      for(m=0;m<N;m++) {
-	y = j + n2 - m;
-
-	if (y < 0)
-	  y = N + y;
-	else if (y > N-1)
-	  y = y - N;
-
-	for(n=0;n<N;n++) {
-	  z = k + n2 - n;
-
-	  if (z < 0)
-	    z = N + z;
-	  else if (z > N-1)
-	    z = z - N;
-
-	  //multiply the weighted fourier coeff product
-	  qHat[index][0] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][0] - fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][1]);
-	  qHat[index][1] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][1] + fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][0]);
-	}
-      }
-    }
-  }
-
-  //End of parallel section
-
-  ifft3D(qHat, fftOut_f);
+  compute_Qhat(conv_weights, g_i, M_j);
   //set Collision output
   for (index = 0; index < N * N * N; index++) {
    Q[index] += fftOut_f[index][0];
-  //second part - quadratic deviations
-    qHat[index][0] = 0.0;
-    qHat[index][1] = 0.0;
-    fftIn_f[index][0] = g_i[index];
-    fftIn_f[index][1] = 0.0;
-    fftIn_g[index][0] = g_j[index];
-    fftIn_g[index][1] = 0.0;
   }
 
-  //move to fourier space
-  fft3D(fftIn_f, fftOut_f);
-  fft3D(fftIn_g, fftOut_g);
-
-
-  #pragma omp parallel for private(i,j,k,l,m,n,x,y,z,conv_weight_chunk)
-  for (index = 0; index < N * N * N; index++) {
-    i = index / (N * N);
-    j = (index - i * N * N) / N;
-    k = index - N * (j + i * N);
-    conv_weight_chunk = conv_weights[index];
-
-    int n2 = N / 2;
-
-    for(l=0;l<N;l++) {
-      x = i + n2 - l;
-      if (x < 0)
-	x = N + x;
-      else if (x > N-1)
-	x = x - N;
-
-      for(m=0;m<N;m++) {
-	y = j + n2 - m;
-
-	if (y < 0)
-	  y = N + y;
-	else if (y > N-1)
-	  y = y - N;
-
-	for(n=0;n<N;n++) {
-	  z = k + n2 - n;
-
-	  if (z < 0)
-	    z = N + z;
-	  else if (z > N-1)
-	    z = z - N;
-
-	  //multiply the weighted fourier coeff product
-	  qHat[index][0] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][0] - fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][1]);
-	  qHat[index][1] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][1] + fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][0]);
-	}
-      }
-    }
-  }
-
-  //End of parallel section
-
-  ifft3D(qHat, fftOut_f);
-
+  compute_Qhat(conv_weights, g_i, g_j);
   //set Collision output
   for (index = 0; index < N * N * N; index++) {
 	Q[index] += fftOut_f[index][0];
@@ -307,64 +197,7 @@ void ComputeQ_maxPreserve(double *f, double *g, double *Q, double **conv_weights
 
   //Maxwellian part
   /*
-  for (index = 0; index < N * N * N; index++) {
-    qHat[index][0] = 0.0;
-    qHat[index][1] = 0.0;
-    fftIn_f[index][0] = M_i[index];
-    fftIn_f[index][1] = 0.0;
-    fftIn_g[index][0] = M_j[index];
-    fftIn_g[index][1] = 0.0;
-  }
-
-  //move to fourier space
-  fft3D(fftIn_f, fftOut_f);
-  fft3D(fftIn_g, fftOut_g);
-
-
-  #pragma omp parallel for private(i,j,k,l,m,n,x,y,z,conv_weight_chunk)
-  for (index = 0; index < N * N * N; index++) {
-    i = index / (N * N);
-    j = (index - i * N * N) / N;
-    k = index - N * (j + i * N);
-    conv_weight_chunk = conv_weights[index];
-
-    int n2 = N / 2;
-
-    for(l=0;l<N;l++) {
-      x = i + n2 - l;
-      if (x < 0)
-	x = N + x;
-      else if (x > N-1)
-	x = x - N;
-
-      for(m=0;m<N;m++) {
-	y = j + n2 - m;
-
-	if (y < 0)
-	  y = N + y;
-	else if (y > N-1)
-	  y = y - N;
-
-	for(n=0;n<N;n++) {
-	  z = k + n2 - n;
-
-	  if (z < 0)
-	    z = N + z;
-	  else if (z > N-1)
-	    z = z - N;
-
-	  //multiply the weighted fourier coeff product
-	  qHat[index][0] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][0] - fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][1]);
-	  qHat[index][1] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][1] + fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][0]);
-	}
-      }
-    }
-  }
-
-  //End of parallel section
-
-  ifft3D(qHat, fftOut_f);
-
+  compute_Qhat(Q, conv_weights, M_i, M_j);
   //set Collision output
   for (index = 0; index < N * N * N; index++) {
     Q[index] += fftOut_f[index][0];
@@ -374,88 +207,13 @@ void ComputeQ_maxPreserve(double *f, double *g, double *Q, double **conv_weights
 
 void ComputeQ(double *f, double *g, double *Q, double **conv_weights)
 {
-  int i, j, k, l, m, n, x, y, z;
-  double *conv_weight_chunk;
+  int index;
 
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++)
-	{
-	     qHat[k + N*(j + N*i)][0] = 0.0;
-	     qHat[k + N*(j + N*i)][1] = 0.0;
-	  fftIn_f[k + N*(j + N*i)][0] = f[k + N*(j + N*i)];
-	  fftIn_f[k + N*(j + N*i)][1] = 0.0;
-	  fftIn_g[k + N*(j + N*i)][0] = g[k + N*(j + N*i)];
-	  fftIn_g[k + N*(j + N*i)][1] = 0.0;
-	}
-
-  //move to fourier space
-  fft3D(fftIn_f, fftOut_f);
-  fft3D(fftIn_g, fftOut_g);
-
-
-  #pragma omp parallel for private(i,j,k,l,m,n,x,y,z,conv_weight_chunk)
-  for(i=0;i<N;i++)
-  for(j=0;j<N;j++)
-  for(k=0;k<N;k++) {
-
-    conv_weight_chunk = conv_weights[k + N*(j + N*i)];
-
-    int n2,n3;
-    if(N % 2 == 0) {
-      n2 = N/2;
-      n3 = 1;
-    }
-    else {
-      n2 = (N-1)/2;
-      n3 = 0;
-    }
-
-    for(l=0;l<N;l++) {
-      x = i + n2 - l;
-      if (x < 0)
-	x = N + x;
-      else if (x > N-1)
-	x = x - N;
-
-      for(m=0;m<N;m++) {
-	y = j + n2 - m;
-
-	if (y < 0)
-	  y = N + y;
-	else if (y > N-1)
-	  y = y - N;
-
-	for(n=0;n<N;n++) {
-	  z = k + n2 - n;
-
-	  if (z < 0)
-	    z = N + z;
-	  else if (z > N-1)
-	    z = z - N;
-
-	  //multiply the weighted fourier coeff product
-	  qHat[k + N*(j + N*i)][0] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][0] - fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][1]);
-	  qHat[k + N*(j + N*i)][1] += conv_weight_chunk[n + N*(m + N*l)]*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][1] + fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][0]);
-
-	  //Timing purposes only
-	  //qHat[k + N*(j + N*i)][0] += 0.0*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][0] - fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][1]);
-	  //qHat[k + N*(j + N*i)][1] += 0.0*(fftOut_g[n + N*(m + N*l)][0]*fftOut_f[z + N*(y + N*x)][1] + fftOut_g[n + N*(m + N*l)][1]*fftOut_f[z + N*(y + N*x)][0]);
-	}
-      }
-    }
-  }
-
-  //End of parallel section
-
-  ifft3D(qHat, fftOut_f);
-
+  compute_Qhat(conv_weights, f, g);
   //set Collision output
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++)	{
-	Q[k + N*(j + N*i)] = fftOut_f[k + N*(j + N*i)][0];
-      }
+  for (index = 0; index < N * N * N; index++) {
+    Q[index] = fftOut_f[index][0];
+  }
 }
 
 
@@ -467,37 +225,38 @@ function fft3D
 --------------
 Computes the fourier transform of in, and adjusts the coefficients based on our v, eta grids
 */
-void fft3D(fftw_complex *in, fftw_complex *out)
-{
+void fft3D(fftw_complex *in, fftw_complex *out) {
   int i, j, k, index;
-  double sum, prefactor;
+  double sum, prefactor, factor;
 
-  prefactor = scale3*dv*dv*dv;
+  prefactor = scale3 * dv * dv * dv;
 
   //shift the 'v' terms in the exponential to reflect our velocity domain
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++)	{
-	  index = k + N*(j + N*i);
-	  sum = (double)(i + j + k) * L_eta*dv;
+  for (index = 0; index < N * N * N; index++) {
+    i = index / (N * N);
+    j = (index - i * N * N) / N;
+    k = index - N * (j + i * N);
+    sum = (double)(i + j + k) * L_eta * dv;
 
-	  //dv correspond to the velocity space scaling - ensures that the FFT is properly scaled since fftw does no scaling at all
-	  temp[index][0] = prefactor*wtN[i]*wtN[j]*wtN[k]*(cos(sum)*in[index][0] - sin(sum)*in[index][1]);
-	  temp[index][1] = prefactor*wtN[i]*wtN[j]*wtN[k]*(cos(sum)*in[index][1] + sin(sum)*in[index][0]);
-      }
+    factor = prefactor * wtN[i] * wtN[j] * wtN[k];
+
+    //dv correspond to the velocity space scaling - ensures that the FFT is properly scaled since fftw does no scaling at all
+    temp[index][0] = factor * (cos(sum)*in[index][0] - sin(sum)*in[index][1]);
+    temp[index][1] = factor * (cos(sum)*in[index][1] + sin(sum)*in[index][0]);
+  }
   //computes fft
   fftw_execute(p_forward);
 
   //shifts the 'eta' terms to reflect our fourier domain
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++) {
-	  index = k + N*(j + N*i);
-	  sum = L_v*(eta[i] + eta[j] + eta[k]);
+  for (index = 0; index < N * N * N; index++) {
+    i = index / (N * N);
+    j = (index - i * N * N) / N;
+    k = index - N * (j + i * N);
+    sum = L_v * (eta[i] + eta[j] + eta[k]);
 
-	  out[index][0] = cos(sum)*temp[index][0] - sin(sum)*temp[index][1];
-	  out[index][1] = cos(sum)*temp[index][1] + sin(sum)*temp[index][0];
-      }
+    out[index][0] = cos(sum)*temp[index][0] - sin(sum)*temp[index][1];
+    out[index][1] = cos(sum)*temp[index][1] + sin(sum)*temp[index][0];
+  }
 
 }
 
@@ -513,31 +272,33 @@ Computes the inverse fourier transform of in, and adjusts the coefficients based
 void ifft3D(fftw_complex *in, fftw_complex *out)
 {
   int i, j, k, index;
-  double sum;
+  double sum, factor;
   double prefactor = deta*deta*deta*scale3;
 
   //shifts the 'eta' terms to reflect our fourier domain
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++) {
-	  index = k + N*(j + N*i);
-	  sum = (double)(i + j + k)*L_v*deta*-1.0;
+  for (index = 0; index < N * N * N; index++) {
+    i = index / (N * N);
+    j = (index - i * N * N) / N;
+    k = index - N * (j + i * N);
+    sum = (double)(i + j + k)*L_v*deta*-1.0;
 
-	  //deta ensures FFT is scaled correctly, since fftw does no scaling at all
-	  temp[index][0] = prefactor*wtN[i]*wtN[j]*wtN[k]*(cos(sum)*in[index][0] - sin(sum)*in[index][1]);
-	  temp[index][1] = prefactor*wtN[i]*wtN[j]*wtN[k]*(cos(sum)*in[index][1] + sin(sum)*in[index][0]);
-      }
+    factor = prefactor * wtN[i] * wtN[j] * wtN[k];
+
+    //deta ensures FFT is scaled correctly, since fftw does no scaling at all
+    temp[index][0] = factor * (cos(sum)*in[index][0] - sin(sum)*in[index][1]);
+    temp[index][1] = factor * (cos(sum)*in[index][1] + sin(sum)*in[index][0]);
+  }
   //compute IFFT
   fftw_execute(p_backward);
 
   //shifts the 'v' terms to reflect our velocity domain
-  for(i=0;i<N;i++)
-    for(j=0;j<N;j++)
-      for(k=0;k<N;k++) {
-	  index = k + N*(j + N*i);
-	  sum = -L_eta*(v[i] + v[j] + v[k]);
+  for (index = 0; index < N * N * N; index++) {
+    i = index / (N * N);
+    j = (index - i * N * N) / N;
+    k = index - N * (j + i * N);
+    sum = -L_eta*(v[i] + v[j] + v[k]);
 
-	  out[index][0] = cos(sum)*temp[index][0] - sin(sum)*temp[index][1];
-	  out[index][1] = cos(sum)*temp[index][1] + sin(sum)*temp[index][0];
-      }
+    out[index][0] = cos(sum)*temp[index][0] - sin(sum)*temp[index][1];
+    out[index][1] = cos(sum)*temp[index][1] + sin(sum)*temp[index][0];
+  }
 }
