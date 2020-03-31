6a6308cc2591b90eb23a6dce9a0bd38f51624010
Jeff Haack <berselius@gmail.com>
2020-03-31 14:59:14 -0500

    Rescale the cutout in the test case

--- src/initializer.c
+++ src/initializer.c
@@ -183,12 +183,13 @@ void initialize_hom(int N, double L_v, double *v, double *zeta, double **f, int 
     double n_cutout = 1.0e23;
     double T_cutout_K = 11000.0;
     double m_cutout = 9.1e-31;
-    
+    double v_th2 = KB * T_coutout_k / m_cutout;
+
     for(j=0;j<N;j++)
       for(k=0;k<N;k++)
 	for(i=0;i<N;i++) {
 	  double v2 = v[i]*v[i] + v[j]*v[j] + v[k]*v[k];
-	  f[spec][k + N*(j + N*i)] = v2 * n_cutout*pow(m_cutout / (2.0 * M_PI * KB * T_cutout_K),1.5) * exp(-0.5*m_cutout*v2 / KB / T_cutout_K);
+	  f[spec][k + N*(j + N*i)] = v2 / v_th2  * n_cutout*pow(m_cutout / (2.0 * M_PI * KB * T_cutout_K),1.5) * exp(-0.5*m_cutout*v2 / KB / T_cutout_K);
 	}
 
     break;
